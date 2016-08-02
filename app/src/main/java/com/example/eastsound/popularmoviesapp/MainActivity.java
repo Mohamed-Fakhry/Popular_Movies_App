c2431a96808600f06cff9c306093db3a164c5ed7
Mohamed Fakhry <mido58890@gmail.com>
2016-08-02 13:27:48 +0200

    ""

--- app/src/main/java/com/example/eastsound/popularmoviesapp/MainActivity.java
+++ app/src/main/java/com/example/eastsound/popularmoviesapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.eastsound.popularmoviesapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.eastsound.popularmoviesapp.service.MovieDataParser;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
